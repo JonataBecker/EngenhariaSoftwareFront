b95a3a1d6feb9dbea6e611acddbfc9396401b2bf
douglascarlos <dnevescarlos@gmail.com>
2016-10-18 22:08:37 -0200

    crud dos tipos de classificação mocado

--- app/classificacaoProjeto/classificacaoProjeto.controller.js
+++ app/classificacaoProjeto/classificacaoProjeto.controller.js
@@ -1,3 +1,47 @@
 angular.module('app').controller('ClassificacaoProjetoController', function ($scope) {
 
+	$scope.classificacoes = [];
+	$scope.classificacao = getNullObject();
+
+	$scope.salvarClassificacao = function(classificacao){
+
+		var idInput = classificacao.id;
+		var descricaoInput = classificacao.descricao;
+		var newClassificacao = {
+			id        : idInput,
+			descricao : descricaoInput
+		};
+		
+		if(idInput == null){
+			newClassificacao.id = Math.floor(Math.random() * (9999 - 1 + 1)) + 1;
+			$scope.classificacoes.push(newClassificacao);
+		}else{
+			$scope.classificacoes.forEach(function (element, index, array){
+				if(element.id == newClassificacao.id) element = newClassificacao;
+			});
+		}
+
+		$scope.classificacao = getNullObject();
+	};
+
+	$scope.excluirClassificacao = function(id){
+		$scope.classificacoes.splice($scope.classificacoes.findIndex(
+			function(element, index, array) {
+				return element.id == id;
+		}), 1);
+	};
+
+	$scope.editarClassificacao = function(id){
+		$scope.classificacao = $scope.classificacoes.find(function (element, index, array) {
+			return element.id == id;
+		});
+	};
+
+	function getNullObject(){
+		return {
+			id:null,
+			descricao:null
+		};
+	}
+
 });
