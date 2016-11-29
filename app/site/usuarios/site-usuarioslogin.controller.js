ed5763055f90b80aa2bde5286e2999b422627212
Lucas Baumgarten <ekbaumgarten@gmail.com>
2016-11-29 21:27:17 -0200

    Parcial - Integração do cadastro com o backend

--- app/site/usuarios/site-usuarioslogin.controller.js
+++ app/site/usuarios/site-usuarioslogin.controller.js
@@ -3,21 +3,30 @@ angular.module('app').controller('SiteUsuariosLoginController', function ($scope
 	$scope.model = {};
 	$scope.error = null;
 	$scope.isLogged = Usuario.isLogged();
-	$scope.user = Usuario.getLoggedUser();
+	$scope.user = null;
+	if ($scope.isLogged) {
+		Usuario.getLoggedUser().then(function(user) {
+			$scope.user = user;
+		});
+	}
 	// $scope.linkTypesList = ;
 	
 	$scope.login = function () {
 		$scope.error = null;
 		$scope.success = null;
-		if (($scope.isLogged = Usuario.login($scope.model.usuario, $scope.model.senha))) {
-			$scope.error = null;
-			$state.go('manager.home');
-		} else {
-			$scope.error = 'Erro ao realizar login. Verifique os dados informados.';
-		}
+		Usuario.login($scope.model.usuario, $scope.model.senha).then(function (a) {
+			console.log(a);
+			if (a) {
+				$scope.error = null;
+				$state.go('manager.home');
+			} else {
+				$scope.error = 'Erro ao realizar login. Verifique os dados informados.';
+			}
+		});
 	}
 	
 	$scope.logout = function () {
+		alert('logout');
 		$scope.error = null;
 		$scope.success = null;
 		Usuario.logout();
@@ -28,11 +37,13 @@ angular.module('app').controller('SiteUsuariosLoginController', function ($scope
 	$scope.passwordRecover = function () {
 		$scope.error = null;
 		$scope.success = null;
-		if (Usuario.sendPassword($scope.model.usuario)) {
-			$scope.success = 'Verifique seu E-mail. Enviamos as instruções para a recuperação da senha.';
-		} else {
-			$scope.error = 'Usuário não encontrado.';
-		}
+		Usuario.sendPassword($scope.model.usuario).then(function (sended) {
+			if (sended) {
+				$scope.success = 'Verifique seu E-mail. Enviamos as instruções para a recuperação da senha.';
+			} else {
+				$scope.error = 'Usuário não encontrado.';
+			}
+		});
 	}
 	
 });
